c42cd2d90ad856455c6644ccf386bab2e4f4029b
ahmad Abdulkhaliq Osman <ahmadabdulkhaliq384@gmail.com>
2018-09-03 21:52:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-function BoardMember(){
-  
+function BoardMember(name,homeState,training){
+  this.name=name
+  this.homeState=homeState
+  thisa.training=training
 }
 
 polishedBoardMember = new BoardMember("Mr. Polished", "New York", "law")
